d553cda39264a6345e1958f2fa683b887fba0623
sar0310 <sar031020@gmail.com>
2023-06-19 15:44:31 -0400

    cambios entidades independientes
    falta declarar las secuencias en cada una de las entidades que estan

--- src/main/java/com/Pruebas/Pruebas/Modelo/Color.java
+++ src/main/java/com/Pruebas/Pruebas/Modelo/Color.java
@@ -1,8 +1,8 @@
 package com.Pruebas.Pruebas.Modelo;
+
 import lombok.Data;
 import lombok.Getter;
 
-import java.math.BigInteger;
 import java.sql.Date;
 import java.time.LocalDate;
 
@@ -12,24 +12,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
 @Data
 @Entity
-@Table(name = "ssa_escuela_samba")
-public class EscuelaSamba {
+@Table(name = "SSA_color")
+public class Color {
     @Id
     private int id;
-    @Column(name="fecha_funda",nullable = false)
-    private Date fecha_funda;
-    @Column(name="nombre",length = 50,nullable = false)
+    @Column(name = "nombre",length = 20,nullable = false)
     private String nombre;
-    @Column(name = "direccion",length = 100,nullable = false)
-    private String direccion;
-    @Column(name = "resumen",length = 500,nullable = false)
-    private String resumen;
-    
-    
 }
--- src/main/java/com/Pruebas/Pruebas/Modelo/TipoEntrada.java
+++ src/main/java/com/Pruebas/Pruebas/Modelo/TipoEntrada.java
@@ -22,14 +22,14 @@ import jakarta.persistence.Table;
 public class TipoEntrada {
     @Id
     private int id;
-    @Column(name="tipo",length = 2,nullable = false)
+    @Column(name="tipo",length = 2,columnDefinition = "varchar(2) not null constraint check_tipo check(tipo in ('GP','GF','AN','SL'))",nullable = false)
     private String tipo;
-    @Column(name="sector",nullable = false)
+    @Column(name="sector",columnDefinition = "numeric(2) not null constraint check_sector check(sector in (2,3,4,5,6,7,9,11))",nullable = false)
     private int sector;
-    @Column(name = "calidad",nullable = false)
+    @Column(name = "calidad",columnDefinition = "numeric(1) not null constraint check_calidad check(calidad>0 and calidad<9)",nullable = false)
     private int calidad;
-    @Column(name = "tipo_desfile",length = 8,nullable = false)
+    @Column(name = "tipo_desfile",length = 8,columnDefinition = "varchar(8) not null constraint check_tipo_desfile check(tipo_desfile in ('acceso','especial','campeon'))",nullable = false)
     private String tipo_desfile;
-    @Column(name = "ubicacion",length = 3)
+    @Column(name = "ubicacion",length = 3,columnDefinition = "varchar(3) constraint check_ubicacion check(ubicacion in ('A','B','C','A/B','C/D'))")
     private String ubicacion;
 }
