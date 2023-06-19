51afcea83347c1abc9eb30c28b97ac50a4928034
Maria Ibarra <mariangelicaibarraluces@gmail.com>
2023-06-19 11:29:02 -0400

    Base del proyecto

---
+++ src/main/java/com/Pruebas/Pruebas/Contacto.java
@@ -0,0 +1,38 @@
+package com.Pruebas.Pruebas;
+
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+
+@Data
+@Entity
+@Table(name = "Contacto")
+public class Contacto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name="nombre", unique=true, length=150)
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private LocalDate fechaRegistro;
+    
+    
+
+    @PrePersist
+    private void asignarFechaRegistro(){
+        fechaRegistro= LocalDate.now();
+    }
+
+}
---
+++ src/main/java/com/Pruebas/Pruebas/ContactoRepository.java
@@ -0,0 +1,18 @@
+package com.Pruebas.Pruebas;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactoRepository extends JpaRepository<Contacto,Integer>{
+
+    List<Contacto> findByNombreContaining(String cadena);
+
+    @Query(value="SELECT * FROM contacto order by id asc", nativeQuery = true)
+    List<Contacto> OrderById();
+
+    Contacto findByNombre(String nombre);
+}
