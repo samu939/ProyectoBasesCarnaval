9f031e742eb4b834de5215bf0b599e0eea309779
ZamudiaJr <129618132+ZamudiaJr@users.noreply.github.com>
2023-06-24 11:27:43 -0400

    registrar cliente en BD

---
+++ src/main/java/com/Pruebas/Pruebas/Controladoras/ClienteController.java
@@ -0,0 +1,31 @@
+package com.Pruebas.Pruebas.Controladoras;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.Pruebas.Pruebas.Modelo.Cliente;
+import com.Pruebas.Pruebas.Repositorios.ClienteRepository;
+
+
+
+@Controller
+public class ClienteController {
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping("/nuevoCliente")
+    public String nuevo(Model model){
+        model.addAttribute("cliente", new Cliente());
+        return "registrarCliente";
+    }
+
+    @PostMapping("/nuevoCliente")
+    public String crear(Cliente cliente){
+        clienteRepository.save(cliente);
+        return "redirect:/";
+    }
+}
